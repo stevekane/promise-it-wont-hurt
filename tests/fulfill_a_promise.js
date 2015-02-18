004b068a6fd6cfd6187a16dfe5bee76efb2cf432
Christophe Porteneuve <tdd@tddsworld.com>
2015-02-18 18:38:58 +0100

    Removed obsolete  subdir, which was no tests at all, but just dups / former versions of exercise solutions, now in  subdirs for each exercise.

--- tests/fulfill_a_promise.js
+++
@@ -1,5 +0,0 @@
-var q = require('q');
-var def = q.defer();
-
-def.promise.then(console.log);
-setTimeout(def.resolve, 300, "RESOLVED!");
--- tests/reject_a_promise.js
+++
@@ -1,9 +0,0 @@
-var q = require('q');
-var def = q.defer();
-
-function printError (err) {
-  console.log(err.message); 
-}
-
-def.promise.then(null, printError);
-setTimeout(def.reject, 300, new Error("REJECTED!"));
