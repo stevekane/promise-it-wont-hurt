adffd4fe3c10bde3913d9a48ec2a1312efde2f70
Timothy Gu <timothygu99@gmail.com>
2016-01-18 17:36:42 -0800

    Remove timer useage from two exercises

--- exercises/always_async/exercise.js
+++ exercises/always_async/exercise.js
@@ -5,18 +5,12 @@ var filecheck = require('workshopper-exercise/filecheck');
 var execute = require('workshopper-exercise/execute');
 var comparestdout = require('workshopper-exercise/comparestdout');
 var wrappedexec = require('@timothygu/workshopper-wrappedexec');
-var timer = require('../../lib/timer');
-
-var THRESHOLD = 100;
 
 // checks that the submission file actually exists
 exercise = filecheck(exercise);
 
 // execute the solution and submission in parallel with spawn()
 exercise = execute(exercise);
-
-// timer
-exercise = timer(exercise, THRESHOLD);
 
 // compare stdout of solution and submission
 exercise = comparestdout(exercise);
