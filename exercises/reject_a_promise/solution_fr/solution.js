1d65af9e86a108a25ebb59fd5fd5741629b01549
Christophe Porteneuve <tdd@tddsworld.com>
2016-01-30 19:43:49 +0100

    L10n FR for reject exercise

--- exercises/reject_a_promise/solution_fr/solution.js
+++ exercises/reject_a_promise/solution_fr/solution.js
@@ -1,9 +1,20 @@
-var q = require('q');
-var def = q.defer();
+'use strict';
 
-function printError (err) {
-  console.log(err.message);
+// Crée la promesse
+var promise = new Promise(function (fulfill, reject) {
+  // Une fois atteint le timeout de 300ms, on rejette la
+  // promesse avec un objet `Error` dont le message est « REJET ! ».
+  setTimeout(reject, 300, new Error('REJET !'))
+})
+
+// Crée la fonction qui affichera `error.message` à l’aide
+// de `console.log`.
+
+function onReject (error) {
+  console.log(error.message);
 }
 
-def.promise.then(null, printError);
-setTimeout(def.reject, 300, new Error("REJETE !"));
+// Passe cette fonction comme gestionnaire de rejet à la méthode
+// `then` de la promesse (le 2e argument).
+
+promise.then(null, onReject);
