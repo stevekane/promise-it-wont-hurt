d4ed0315c9140d87bfcd062d3e2c0600d1cef37d
Adam Boro <adam@adamboro.com>
2016-09-14 17:03:58 -0400

    Fix for exercise 9 (#98)
    Fixes #81 and #92.

--- exercises/throw_an_error/exercise.js
+++ exercises/throw_an_error/exercise.js
@@ -1,8 +1,14 @@
 var exercise      = require('workshopper-exercise')()
   , filecheck     = require('workshopper-exercise/filecheck')
   , execute       = require('workshopper-exercise/execute')
-  , comparestdout = require('workshopper-exercise/comparestdout')
+  , split         = require('split')
+  , chalk         = require('chalk')
+  , tuple         = require('tuple-stream')
+  , through2      = require('through2')
 
+function repeat (ch, sz) {
+  return new Array(sz + 1).join(ch)
+}
 
 // checks that the submission file actually exists
 exercise = filecheck(exercise)
@@ -11,7 +17,21 @@ exercise = filecheck(exercise)
 exercise = execute(exercise)
 
 // compare stdout of solution and submission
-exercise = comparestdout(exercise)
+exercise.addProcessor(function (mode, callback) {
+  var outputStream = through2.obj(function (chunk) {
+    var result = chunk[0] === chunk[1]
+    if (!result) {
+      outputStream.push(chalk.yellow(repeat('\u2500', 80)) + '\n')
+      outputStream.push('actual: ' + chalk.red(chunk[0]) + '\n')
+      outputStream.push('expected: ' + chalk.red(chunk[1]) + '\n')
+      outputStream.push(chalk.yellow(repeat('\u2500', 80)) + '\n\n')
+    }
+    callback(null, result)
+  })
+  tuple(this.submissionStdout.pipe(split()), this.solutionStdout.pipe(split()))
+    .pipe(outputStream)
+    .pipe(process.stdout)
+})
 
 exercise.addSetup(function (mode, callback) {
   this.submissionArgs = this.solutionArgs = ["{ohcrap: 'our data!'"]
