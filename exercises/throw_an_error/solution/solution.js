5081f5abd86d522bdafb7368a66e5ab1f80cfa44
Anne Klapwijk <anne.klapwijk@gmail.com>
2017-10-22 19:48:05 +0200

    use a a rejection handler as a callback

--- exercises/throw_an_error/solution/solution.js
+++ exercises/throw_an_error/solution/solution.js
@@ -5,10 +5,14 @@ function parsePromised(json) {
     try {
       fulfill(JSON.parse(json));
     } catch (e) {
-      reject(e.message);
+      reject(e);
     }
   });
 }
 
+function onReject(error) {
+  console.log(error.message);
+}
+
 parsePromised(process.argv[2])
-.then(null, console.log);
+.then(null, onReject);
